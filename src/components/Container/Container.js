b6fac1878ce3111ccfa5a44e1e1aa13b4e71d2b7
Ariel Sanchez <freska_96@hotmail.com>
2024-10-15 13:02:07 -0600

    feat: ediciones

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,11 @@
-import { Container as Content } from 'semantic-ui-react';
-import './Container.scss';
+import { Container as Content } from "semantic-ui-react";
+
+import "./Container.scss";
 
 export default function Container({ children, bg }) {
     return (
-        <div className={bg !== 'light' ? 'container-bg' : null}>
+        <div className={bg ? "" : "container-bg"}>
             <Content>{children}</Content>
         </div>
     );
-};
+}
